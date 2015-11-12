9e309d25f5eb5816ee1692100b8bc0efce599921
sangrampp <sangrampp@gmail.com>
2015-11-12 15:35:40 +0530

    Done setup of the app

---
+++ app/js/src/app.js
@@ -0,0 +1,2 @@
+Parse.initialize("YPVBaH35TO8x3EPbBE9Pvks1JfWBBiIsYdPP4rpI", "c1XN80bwPHgIWXpTsIEsjovkOTpN0R6iR5TnLb1n");
+angular.module("Xpens-Track", []);
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp        = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var bowerDir = './bower_components';
+
+var jsFiles = [
+    './bower_components/angular/angular.js',
+    './bower_components/angular-ui-router/release/angular-ui-router.js',
+    'app/js/src/app.js',
+    'app/js/src/config/RouteConfig.js',
+    'app/js/src/config/StateChange.js',   
+    'app/js/src/services/ParseService.js',     
+    'app/js/src/controllers/LoginController.js',
+    'app/js/src/controllers/UserController.js',
+    'app/js/src/controllers/ExpenseController.js'   
+]
+
+var cssFiles = [
+    './bower_components/bootswatch/flatly/bootstrap.css',    
+    'app/css/src/app.css' 
+]
+
+  gulp.task('scripts', function(){
+        return gulp.src(jsFiles)
+            .pipe(concat('main.js'))
+            .pipe(gulp.dest('app/js'));
+    });
+
+  gulp.task('sass', function () {
+    return gulp.src(cssFiles)
+      .pipe(concat('main.css'))      
+      .pipe(gulp.dest('app/css'));
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch('app/js/src/*.js', ['scripts']);
+    gulp.watch('app/js/src/**/*.js', ['scripts']);
+    gulp.watch('app/css/src/*.css', ['sass']);
+  });
+
+// gulp.task('default', ['sass', 'serve']);
+gulp.task('default', ['scripts', 'sass', 'watch']);
